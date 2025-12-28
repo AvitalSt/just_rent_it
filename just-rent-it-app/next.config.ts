fc0b80ade85a09d355266ce94ca83533cbe206d9
AvitalSt <154595696+AvitalSt@users.noreply.github.com>
2025-12-28 19:09:19 +0200

    fix: google auth headers and image patterns

--- just-rent-it-app/next.config.ts
+++ just-rent-it-app/next.config.ts
@@ -2,6 +2,21 @@ import type { NextConfig } from "next";
 
 const nextConfig = {
   reactStrictMode: false,
+
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin-allow-popups',
+          },
+        ],
+      },
+    ];
+  },
+  
   images: {
     remotePatterns: [
       {

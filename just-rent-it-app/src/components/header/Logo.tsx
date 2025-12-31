343719554dbf67588d2c561da8608ca5334885a5
AvitalSt <154595696+AvitalSt@users.noreply.github.com>
2025-12-31 13:36:38 +0200

    fix: organize code

--- just-rent-it-app/src/components/header/Logo.tsx
+++ just-rent-it-app/src/components/header/Logo.tsx
@@ -3,15 +3,16 @@ import Link from "next/link";
 
 export default function Logo() {
   return (
-  <Link
+    <Link
       href="/"
       className="absolute left-1/2 transform -translate-x-1/2 cursor-pointer"
-    >      <Image
+    >
+      <Image
         src="/images/logo-new.png"
         alt="Just Rent It Logo"
         width={80}
         height={40}
-        style={{ width: "auto" }} 
+        style={{ width: "auto" }}
         priority // גורם לנקסט לטוען את התמונה מהר יותר כי זה קובץ לוגו
       />
     </Link>

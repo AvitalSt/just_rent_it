ebc4bc32de21042d16e3802f127c4ecd1403aa7d
AvitalSt <154595696+AvitalSt@users.noreply.github.com>
2026-02-09 11:04:28 +0200

    fix: use ISR to revalidate home page most viewed dresses daily

--- just-rent-it-app/src/components/home/Home.tsx
+++ just-rent-it-app/src/components/home/Home.tsx
@@ -6,21 +6,10 @@ import CategoriesSection from "./CategoriesSection";
 import HowItWorksSection from "./HowItWorksSection";
 import ViewMoreSection from "./ViewMoreSection";
 
-let mostViewedCache: any = null;
-let cacheTime: number = 0;
-const CACHE_TTL = 1000 * 60 * 60 * 24; // 24 שעות
+export const revalidate = 60 * 60 * 24;
 
 export default async function Home() {
-  const now = Date.now();
-  let mostViewed;
-
-  if (mostViewedCache && now - cacheTime < CACHE_TTL) {
-    mostViewed = mostViewedCache; 
-  } else {
-    mostViewed = await getMostViewedDresses(); 
-    mostViewedCache = mostViewed;
-    cacheTime = now;
-  }
+  const mostViewed = await getMostViewedDresses();
 
   return (
     <div className="bg-white" dir="rtl">
